2aef670e8ede2e104f6dd6b906f7336f1198caaf
Patrick Weibel <p.weibel@eweibel.net>
2010-04-05 14:09:07 +0200

    Fixed mappings

--- DinX.Common/Domain/Task.cs
+++ DinX.Common/Domain/Task.cs
@@ -9,6 +9,7 @@ namespace DinX.Common.Domain
         public virtual string Title { get; set; }
         public virtual string Description { get; set; }
         public virtual Project Project { get; set; }
+        public virtual Sprint Sprint { get; set; }
         public virtual IList<User> Users { get; set; }
     }
 }
